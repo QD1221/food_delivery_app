0ced46200e10df156ad0255925b2bf7a1fa79dbb
QD1221 <zxcvbnm12122000@gmail.com>
2021-08-24 19:07:11 +0700

    3rd commit

--- lib/ui/order_page.dart
+++ lib/ui/order_page.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class OrderPage extends StatefulWidget {
@@ -19,7 +20,11 @@ class _OrderPageState extends State<OrderPage> {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                CircleAvatar(),
+                CircleAvatar(
+                  backgroundColor: Colors.red[400],
+                  foregroundColor: Colors.white,
+                  child: Icon(Icons.location_on_outlined),
+                ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8),
                   child: Column(
@@ -37,7 +42,10 @@ class _OrderPageState extends State<OrderPage> {
                   ),
                 ),
                 Spacer(),
-                IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                ),
                 CircleAvatar(),
               ],
             ),
@@ -45,7 +53,101 @@ class _OrderPageState extends State<OrderPage> {
         ),
         Expanded(
           flex: 15,
-          child: Placeholder(),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: SizedBox(
+                    height: 200,
+                    child: Swiper(
+                      outer: false,
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red[200],
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: Text('$index'),
+                          ),
+                        );
+                      },
+                      pagination: SwiperPagination(
+                          builder: DotSwiperPaginationBuilder(
+                        activeColor: Colors.grey,
+                        color: Colors.white,
+                      )),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  child: Text(
+                    'Eat what makes you happy',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8, top: 8, bottom: 8),
+                  child: SizedBox(
+                    height: 84,
+                    child: ListView.builder(
+                      itemCount: 8,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 32,
+                                backgroundColor: Colors.red[100],
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                'Healthy',
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: Row(
+                    children: [
+                      Text(
+                        '127 restaurants around you',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.import_export_outlined,
+                        size: 18,
+                      ),
+                      Text(
+                        'Popular',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ],
     );
