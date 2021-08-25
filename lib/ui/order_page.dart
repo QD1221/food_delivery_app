04dc66dcd5566d2fef6b7372774946f9caf9a80d
QD1221 <zxcvbnm12122000@gmail.com>
2021-08-25 11:00:29 +0700

    4th commit

--- lib/ui/order_page.dart
+++ lib/ui/order_page.dart
@@ -1,5 +1,6 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class _OrderPageState extends State<OrderPage> {
         Expanded(
           flex: 2,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
@@ -70,31 +71,115 @@ class _OrderPageState extends State<OrderPage> {
                           padding: const EdgeInsets.symmetric(horizontal: 8),
                           child: Container(
                             decoration: BoxDecoration(
-                              color: Colors.red[200],
+                              gradient: LinearGradient(colors: [
+                                Colors.red[400]!,
+                                Colors.red[300]!,
+                                Colors.red[200]!,
+                                Colors.red[100]!,
+                              ]),
                               borderRadius: BorderRadius.circular(24),
                             ),
-                            child: Text('$index'),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  left: 8,
+                                  top: 16,
+                                  bottom: 16,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Up To',
+                                        style: GoogleFonts.metrophobic(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      Text(
+                                        '70% OFF',
+                                        style: GoogleFonts.metrophobic(
+                                          color: Colors.white,
+                                          fontSize: 32,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        'with free delivery',
+                                        style: GoogleFonts.metrophobic(
+                                          color: Colors.white,
+                                          fontSize: 24,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        child: Divider(
+                                          color: Colors.white,
+                                        ),
+                                        width: 64,
+                                      ),
+                                      Text(
+                                        'no COOKing',
+                                        style: GoogleFonts.molle(
+                                          color: Colors.white,
+                                          fontSize: 24,
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            'July',
+                                            style: GoogleFonts.molle(
+                                              color: Colors.white,
+                                              fontSize: 24,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          CircleAvatar(
+                                            backgroundColor: Colors.white,
+                                            foregroundColor: Colors.black,
+                                            radius: 12,
+                                            child: Icon(
+                                              Icons.arrow_forward,
+                                              size: 14,
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Positioned(
+                                  child:
+                                      Image.asset('assets/fd_main_img_01.png'),
+                                  right: 8,
+                                  bottom: 0,
+                                ),
+                              ],
+                            ),
                           ),
                         );
                       },
                       pagination: SwiperPagination(
-                          builder: DotSwiperPaginationBuilder(
-                        activeColor: Colors.grey,
-                        color: Colors.white,
-                      )),
+                        builder: DotSwiperPaginationBuilder(
+                          activeColor: Colors.grey,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                   child: Text(
                     'Eat what makes you happy',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: 8, top: 8, bottom: 8),
+                  padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
                   child: SizedBox(
                     height: 84,
                     child: ListView.builder(
@@ -106,7 +191,9 @@ class _OrderPageState extends State<OrderPage> {
                             children: [
                               CircleAvatar(
                                 radius: 32,
-                                backgroundColor: Colors.red[100],
+                                backgroundColor: Colors.red[100]!,
+                                backgroundImage: NetworkImage(
+                                    "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg"),
                               ),
                               SizedBox(
                                 height: 4,
@@ -125,7 +212,7 @@ class _OrderPageState extends State<OrderPage> {
                 ),
                 Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   child: Row(
                     children: [
                       Text(
@@ -143,6 +230,113 @@ class _OrderPageState extends State<OrderPage> {
                             fontWeight: FontWeight.bold, fontSize: 12),
                       ),
                     ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Container(
+                    height: 320,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://cdn.pixabay.com/photo/2016/03/09/12/07/meal-1246287_960_720.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          flex: 6,
+                        ),
+                        Divider(),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Sultan Kacchi Biryani',
+                                      style: GoogleFonts.metrophobic(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 4),
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            '4.3',
+                                            style: GoogleFonts.metrophobic(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Icon(
+                                            Icons.star,
+                                            color: Colors.yellow,
+                                            size: 12,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Biryani, Desserts, Kacchi',
+                                      style: GoogleFonts.metrophobic(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Text(
+                                      'Price Range \$12.00 - \$14.00',
+                                      style: GoogleFonts.metrophobic(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Divider(),
+                                Wrap(
+                                  children: [
+                                    Text(
+                                        'Zomato funds environmental projects to offset delivery carbon footprint'),
+
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          flex: 4,
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
